34c17f10f508fd39951347fee93c7530902fa823
CedMaire <cedric.maire@epfl.ch>
2017-09-27 09:31:44 +0200

    Initial app project setup

---
+++ cpmac/app/bundle-config.js
@@ -0,0 +1,14 @@
+if (global.TNS_WEBPACK) {
+    // registers tns-core-modules UI framework modules
+    require("bundle-entry-points");
+
+    // register application modules
+    global.registerModule("nativescript-telerik-ui/sidedrawer",
+        () => require("../node_modules/nativescript-telerik-ui/sidedrawer"));
+
+    global.registerModule("shared/my-drawer/MyDrawer", () => require("./shared/my-drawer/MyDrawer"));
+    global.registerModule("home/home-page", () => require("./drawers/home/home-page"));
+    global.registerModule("pop/pop-page", () => require("./drawers/pop/pop-page"));
+    global.registerModule("cisc/cisc-page", () => require("./drawers/cisc/cisc-page"));
+    global.registerModule("settings/settings-page", () => require("./drawers/settings/settings-page"));
+}
---
+++ cpmac/app/shared/my-drawer/MyDrawer-view-model.js
@@ -0,0 +1,47 @@
+const observableModule = require("data/observable");
+
+/* ***********************************************************
+ * Keep data that is displayed in your app drawer in the MyDrawer custom component view model.
+ *************************************************************/
+function MyDrawerViewModel(selectedPage) {
+    const viewModel = observableModule.fromObject({
+        /* ***********************************************************
+         * Use the MyDrawer view model to initialize the properties data values.
+         * The navigationItems property is initialized here and is data bound to <ListView> in the MyDrawer view file.
+         * Add, remove or edit navigationItems to change what is displayed in the app drawer list.
+         *************************************************************/
+        navigationItems: [{
+                title: "Home",
+                name: "home",
+                route: "drawers/home/home-page",
+                icon: "\uf015",
+                isSelected: selectedPage === "Home"
+            },
+            {
+                title: "PoP",
+                name: "pop",
+                route: "drawers/pop/pop-page",
+                icon: "\uf0a1",
+                isSelected: selectedPage === "PoP"
+            },
+            {
+                title: "CISC",
+                name: "cisc",
+                route: "drawers/cisc/cisc-page",
+                icon: "\uf0c1",
+                isSelected: selectedPage === "CISC"
+            },
+            {
+                title: "Settings",
+                name: "settings",
+                route: "drawers/settings/settings-page",
+                icon: "\uf013",
+                isSelected: selectedPage === "Settings"
+            }
+        ]
+    });
+
+    return viewModel;
+}
+
+module.exports = MyDrawerViewModel;
