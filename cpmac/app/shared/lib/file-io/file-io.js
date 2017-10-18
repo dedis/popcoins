b2745da1d313c4cc8ebd30f863711ce46b08f98a
CedMaire <cedric.maire@epfl.ch>
2017-10-18 11:53:41 +0200

    Commented the FileIO library

--- cpmac/app/shared/lib/file-io/file-io.js
+++ cpmac/app/shared/lib/file-io/file-io.js
@@ -7,8 +7,14 @@ const FileSystem = require("tns-core-modules/file-system");
 const Documents = FileSystem.knownFolders.currentApp();
 
 // Functions ------------------------------------------------------------------
-function getContentOf(file) {
-  return Documents.getFile(file)
+
+/**
+ * Gets the content of the file at filePath and return a promise with the content.
+ * @param filePath - the path to the file
+ * @returns {Promise.<string>}
+ */
+function getContentOf(filePath) {
+  return Documents.getFile(filePath)
                   .readText()
                   .catch((error) => {
                     console.log("READING ERROR:");
@@ -16,9 +22,15 @@ function getContentOf(file) {
                   });
 }
 
-function writeContentTo(file, string) {
-  return Documents.getFile(file)
-                  .writeText(string)
+/**
+ * Writes the content to the file at filePath and returns a promise of any. This method overwrites the file completely.
+ * @param filePath - the path to the file
+ * @param content - the content to write
+ * @returns {Promise.<any>}
+ */
+function writeContentTo(filePath, content) {
+  return Documents.getFile(filePath)
+                  .writeText(content)
                   .catch((error) => {
                     console.log("WRITING ERROR:");
                     console.dir(error);
