e8fe3f22acd996c20bfa796e6e8e6bc405cff395
CedMaire <cedric.maire@epfl.ch>
2017-10-21 00:17:48 +0200

    Started to implement the key registration process for the organizer

--- cpmac/app/drawers/pop/org/register/register-page.js
+++ cpmac/app/drawers/pop/org/register/register-page.js
@@ -1,12 +1,72 @@
+const Dialog = require("ui/dialogs");
+const SwipeToDelete = require("~/shared/lib/ios-swipe-delete/ios-swipe-delete");
+
 const RegisterViewModel = require("./register-view-model");
 
-function onNavigatingTo(args) {
+const registerViewModel = new RegisterViewModel();
+const myRegisteredKeys = registerViewModel.registeredKeys;
+
+function onLoaded(args) {
   if (args.isBackNavigation) {
     return;
   }
 
   const page = args.object;
-  page.bindingContext = new RegisterViewModel();
+
+  // TODO: implement item delete for Android
+  if (page.ios) {
+    let listView = page.getViewById("list-view-registered-keys");
+
+    SwipeToDelete.enable(listView, function (index) {
+      myRegisteredKeys.deleteByIndex(index);
+    });
+  }
+
+  page.bindingContext = registerViewModel;
+
+  loadRegisteredKeys();
 }
 
-exports.onNavigatingTo = onNavigatingTo;
+/**
+ * Loads the registered public keys of the attendees into the list.
+ */
+function loadRegisteredKeys() {
+  myRegisteredKeys.clear();
+  myRegisteredKeys.load();
+}
+
+/**
+ * Function that gets called when the user wants to register a public key manually.
+ */
+function addManual() {
+  Dialog.prompt({
+                  title: "Public Key",
+                  message: "Please enter the public key of an attendee.",
+                  okButtonText: "Register",
+                  cancelButtonText: "Cancel",
+                  inputType: Dialog.inputType.text
+                })
+        .then(args => {
+          if (args.result) {
+            return myRegisteredKeys.addKey(args.text);
+          }
+        });
+}
+
+/**
+ * Function that gets called when the user wants to register a public key by scanning it.
+ */
+function addScan() {
+}
+
+/**
+ * Function that gets called when the user wants to delete the whole list of registered keys.
+ */
+function empty() {
+  myRegisteredKeys.empty();
+}
+
+exports.onLoaded = onLoaded;
+exports.addManual = addManual;
+exports.addScan = addScan;
+exports.empty = empty;
--- cpmac/app/drawers/pop/org/register/register-view-model.js
+++ cpmac/app/drawers/pop/org/register/register-view-model.js
@@ -1,9 +1,109 @@
 const ObservableModule = require("data/observable");
+const ObservableArray = require("data/observable-array").ObservableArray;
+const FilesPath = require("~/shared/res/files/files-path");
+const FileIO = require("~/shared/lib/file-io/file-io");
+
+const viewModel = ObservableModule.fromObject({
+                                                registeredKeys: new ObservableArray()
+                                              });
+
+const myRegisteredKeys = viewModel.registeredKeys;
+const EOL_REGEX = /[\r\n]+/;
 
 function RegisterViewModel() {
-  const viewModel = ObservableModule.fromObject({});
+  setUpRegisteredKeys();
 
   return viewModel;
+}
+
+/**
+ * Sets up the the registeredKeys list with all the needed functions to manage it.
+ */
+function setUpRegisteredKeys() {
+
+  /**
+   * Loads the list of registered keys.
+   * @returns {*|Promise.<any>}
+   */
+  myRegisteredKeys.load = function () {
+    return FileIO.getContentOf(FilesPath.POP_REGISTERED_KEYS)
+                 .then(content => {
+                   return content.split(EOL_REGEX);
+                 })
+                 .then(keysArray => {
+                   for (let i = 0; i < keysArray.length; ++i) {
+                     myRegisteredKeys.push({
+                                             key: keysArray[i]
+                                           });
+                   }
+
+                   return Promise.resolve();
+                 });
+  };
+
+  /**
+   * Adds a new key to the list of registered keys.
+   * @param key - the string of the public key
+   * @returns {*|Promise.<any>}
+   */
+  myRegisteredKeys.addKey = function (key) {
+    // TODO: check if the key has a valid format, do not add same key twice
+    myRegisteredKeys.push({
+                            key: key
+                          });
+
+    return saveKeysToFile();
+  };
+
+  /**
+   * Empties the list completely by deleting them permanently.
+   * @returns {*|Promise.<any>}
+   */
+  myRegisteredKeys.empty = function () {
+    while (myRegisteredKeys.length) {
+      myRegisteredKeys.pop();
+    }
+
+    return FileIO.writeContentTo(FilesPath.POP_REGISTERED_KEYS, "");
+  };
+
+  /**
+   * Clears the list of registered keys, but only in the memory. The permanently stored keys are not affected by
+   * this function.
+   */
+  myRegisteredKeys.clear = function () {
+    while (myRegisteredKeys.length) {
+      myRegisteredKeys.pop();
+    }
+  };
+
+  /**
+   * Deletes a specific key at a given index.
+   * @param index - the index of the key to delete
+   * @returns {*|Promise.<any>}
+   */
+  myRegisteredKeys.deleteByIndex = function (index) {
+    if (0 <= index && index < myRegisteredKeys.length) {
+      myRegisteredKeys.splice(index, 1);
+
+      return saveKeysToFile();
+    } else {
+      return Promise.reject();
+    }
+  };
+}
+
+/**
+ * Function to save the public keys of the attendees permanently.
+ * @returns {*|Promise.<any>}
+ */
+function saveKeysToFile() {
+  const linesOfKeys = myRegisteredKeys.map(keyObject => {
+                                        return keyObject.key;
+                                      })
+                                      .join("\n");
+
+  return FileIO.writeContentTo(FilesPath.POP_REGISTERED_KEYS, linesOfKeys);
 }
 
 module.exports = RegisterViewModel;
