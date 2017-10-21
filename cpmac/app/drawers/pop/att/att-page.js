9742884db6c61bd6ec8d891668748705b52d6b7f
CedMaire <cedric.maire@epfl.ch>
2017-10-21 20:21:43 +0200

    Prepared the pop token generation

--- cpmac/app/drawers/pop/att/att-page.js
+++ cpmac/app/drawers/pop/att/att-page.js
@@ -1,18 +1,33 @@
 const Dialog = require("ui/dialogs");
 const FilesPath = require("~/shared/res/files/files-path");
 const FileIO = require("~/shared/lib/file-io/file-io");
-
 const Frame = require("ui/frame");
 
 const AttViewModel = require("./att-view-model");
 
-function onNavigatingTo(args) {
+let textFieldPrivateKey = undefined;
+
+function onLoaded(args) {
   if (args.isBackNavigation) {
     return;
   }
 
   const page = args.object;
+
+  loadViews(page);
+  if (textFieldPrivateKey === undefined) {
+    throw new Error("a field is undefined, but it shouldn't");
+  }
+
   page.bindingContext = new AttViewModel();
+}
+
+/**
+ * Loads the needed views of the text fields.
+ * @param page - the current page object
+ */
+function loadViews(page) {
+  textFieldPrivateKey = page.getViewById("text-field-private-key");
 }
 
 /**
@@ -43,5 +58,43 @@ function displayQrOfPublicKey() {
                });
 }
 
-exports.onNavigatingTo = onNavigatingTo;
+function generatePopToken() {
+  const privateKey = textFieldPrivateKey.text;
+
+  return FileIO.getContentOf(FilesPath.POP_FINAL_TOML)
+               .then(finalToml => {
+                 if (privateKey.length > 0 && finalToml.length > 0) {
+                   return Dialog.confirm({
+                                           title: "PoP Token Generation",
+                                           message: "Private Key: " + privateKey + "\nFinal Toml: " + finalToml,
+                                           okButtonText: "Generate",
+                                           cancelButtonText: "Cancel"
+                                         })
+                                .then(result => {
+                                  if (result) {
+                                    textFieldPrivateKey.text = "";
+                                    // TODO: generate the PoP Token
+                                  } else {
+                                    return Promise.reject();
+                                  }
+                                })
+                                .catch(() => {
+                                  // The user cancelled the PoP token generation.
+                                });
+                 } else {
+                   return Promise.reject();
+                 }
+               })
+               .catch(() => {
+                 return Dialog.alert({
+                                       title: "Provide More Information",
+                                       message: "Please provide a private key and store the text of your final.toml" +
+                                                " in the settings.",
+                                       okButtonText: "Ok"
+                                     });
+               });
+}
+
+exports.onLoaded = onLoaded;
 exports.displayQrOfPublicKey = displayQrOfPublicKey;
+exports.generatePopToken = generatePopToken;
