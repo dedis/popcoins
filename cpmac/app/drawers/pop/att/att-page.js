229a2d6271cfaeb1f096ce92a742a6b09c23d28e
CedMaire <cedric.maire@epfl.ch>
2017-10-21 21:14:00 +0200

    Prepared the key pair generation process

--- cpmac/app/drawers/pop/att/att-page.js
+++ cpmac/app/drawers/pop/att/att-page.js
@@ -2,6 +2,7 @@ const Dialog = require("ui/dialogs");
 const FilesPath = require("~/shared/res/files/files-path");
 const FileIO = require("~/shared/lib/file-io/file-io");
 const Frame = require("ui/frame");
+const Clipboard = require("nativescript-clipboard");
 
 const AttViewModel = require("./att-view-model");
 
@@ -28,6 +29,80 @@ function onLoaded(args) {
  */
 function loadViews(page) {
   textFieldPrivateKey = page.getViewById("text-field-private-key");
+}
+
+/**
+ * Function that gets called when the user wants to generate a new key pair. It guides the user through the process.
+ * @returns {Promise.<any>}
+ */
+function generateKeyPair() {
+
+  /**
+   * Generates the new key pair and informs the user.
+   * @returns {Promise.<any>}
+   */
+  function generateKeyPair() {
+    // TODO: actually generate the key pair
+    const newPublicKey = "public.key";
+    const newPrivateKey = "private.key";
+
+    return FileIO.writeContentTo(FilesPath.POP_PUBLIC_KEY, newPublicKey)
+                 .then(() => {
+                   return Dialog.confirm({
+                                           title: "New Key Pair",
+                                           message: "The public key has been stored in your settings. Please store" +
+                                                    " your private key securely, after dismissing this message you" +
+                                                    " won't be able to retrieve the private key through the" +
+                                                    " app.\n\nPublic Key:\n" + newPublicKey + "\nPrivate Key:\n" +
+                                                    newPrivateKey,
+                                           okButtonText: "Dismiss",
+                                           cancelButtonText: "Copy Private Key to Clipboard & Dismiss"
+                                         })
+                                .then(result => {
+                                  if (result) {
+                                    return Promise.resolve();
+                                  } else {
+                                    return Clipboard.setText(newPrivateKey);
+                                  }
+                                });
+                 });
+  }
+
+  return FileIO.getContentOf(FilesPath.POP_PUBLIC_KEY)
+               .then(storedPublicKey => {
+                 if (storedPublicKey.length > 0) {
+                   return Dialog.confirm({
+                                           title: "Old Key Pair Overwriting",
+                                           message: "There is already a public key stored in you settings. Do you" +
+                                                    " want to overwrite it and generate a new key pair?",
+                                           okButtonText: "New",
+                                           cancelButtonText: "Cancel"
+                                         })
+                                .then(result => {
+                                  if (result) {
+                                    return generateKeyPair();
+                                  } else {
+                                    return Promise.resolve();
+                                  }
+                                })
+                                .catch(() => {
+                                  return Dialog.alert({
+                                                        title: "Key Pair Generation Error",
+                                                        message: "An unexpected error occurred. Please try again.",
+                                                        okButtonText: "Ok"
+                                                      });
+                                });
+                 } else {
+                   return generateKeyPair();
+                 }
+               })
+               .catch(() => {
+                 return Dialog.alert({
+                                       title: "Key Pair Generation Error",
+                                       message: "An unexpected error occurred. Please try again.",
+                                       okButtonText: "Ok"
+                                     });
+               });
 }
 
 /**
@@ -58,6 +133,11 @@ function displayQrOfPublicKey() {
                });
 }
 
+/**
+ * Function called when the user clicks on the "generate pop token" button. When the user provided all needed
+ * information the pop token will be generated.
+ * @returns {Promise.<any>}
+ */
 function generatePopToken() {
   const privateKey = textFieldPrivateKey.text;
 
@@ -66,7 +146,7 @@ function generatePopToken() {
                  if (privateKey.length > 0 && finalToml.length > 0) {
                    return Dialog.confirm({
                                            title: "PoP Token Generation",
-                                           message: "Private Key: " + privateKey + "\nFinal Toml: " + finalToml,
+                                           message: "Private Key:\n" + privateKey + "\nFinal Toml:\n" + finalToml,
                                            okButtonText: "Generate",
                                            cancelButtonText: "Cancel"
                                          })
@@ -75,11 +155,8 @@ function generatePopToken() {
                                     textFieldPrivateKey.text = "";
                                     // TODO: generate the PoP Token
                                   } else {
-                                    return Promise.reject();
+                                    return Promise.resolve();
                                   }
-                                })
-                                .catch(() => {
-                                  // The user cancelled the PoP token generation.
                                 });
                  } else {
                    return Promise.reject();
@@ -96,5 +173,6 @@ function generatePopToken() {
 }
 
 exports.onLoaded = onLoaded;
+exports.generateKeyPair = generateKeyPair;
 exports.displayQrOfPublicKey = displayQrOfPublicKey;
 exports.generatePopToken = generatePopToken;
