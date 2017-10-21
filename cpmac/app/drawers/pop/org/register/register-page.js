d4e46155474a15672e30878691d0614e7a470114
CedMaire <cedric.maire@epfl.ch>
2017-10-21 13:09:36 +0200

    Auto QR scan added + working

--- cpmac/app/drawers/pop/org/register/register-page.js
+++ cpmac/app/drawers/pop/org/register/register-page.js
@@ -1,7 +1,9 @@
 const Dialog = require("ui/dialogs");
 const SwipeToDelete = require("~/shared/lib/ios-swipe-delete/ios-swipe-delete");
-
+const BCScanner = require("nativescript-barcodescanner").BarcodeScanner;
 const RegisterViewModel = require("./register-view-model");
+
+const BarCodeScanner = new BCScanner();
 
 const registerViewModel = new RegisterViewModel();
 const myRegisteredKeys = registerViewModel.registeredKeys;
@@ -47,7 +49,7 @@ function addManual() {
                   inputType: Dialog.inputType.text
                 })
         .then(args => {
-          if (args.result) {
+          if (args.result && args.text !== undefined && args.text.length > 0) {
             return myRegisteredKeys.addKey(args.text);
           }
         });
@@ -57,6 +59,73 @@ function addManual() {
  * Function that gets called when the user wants to register a public key by scanning it.
  */
 function addScan() {
+  BarCodeScanner.available().then(function (available) {
+    if (available) {
+      availableFunction();
+    } else {
+      notAvailableFunction();
+    }
+  });
+
+  /**
+   * Function that gets executed when there is a camera available on the users phone.
+   */
+  function availableFunction() {
+    /**
+     * Called every time a code was scanned.
+     * @param scanResult - the result of the scan
+     * @returns {*|Promise.<any>}
+     */
+    const continuousCallback = function (scanResult) {
+      // TODO: catch error when not added
+      return myRegisteredKeys.addKey(scanResult.text)
+                             .then(() => {
+                               return BarCodeScanner.stop();
+                             });
+    };
+
+    /**
+     * Called when the scan session terminates.
+     */
+    const closeCallback = function () {
+      // Unused
+    };
+
+    BarCodeScanner.scan({
+                          message: "Scan the public keys of the attendees.",
+                          showFlipCameraButton: true,
+                          showTorchButton: true,
+                          resultDisplayDuration: 1000,
+                          openSettingsIfPermissionWasPreviouslyDenied: true,
+                          beepOnScan: true,
+                          continuousScanCallback: continuousCallback,
+                          closeCallback: closeCallback
+                        })
+                  .then(function () {
+                    // Unused
+                  }, function (error) {
+                    /*
+                     Dialog.alert({
+                     title: "Please try again!",
+                     message: "An error occurred.",
+                     okButtonText: "Ok"
+                     });
+                     */
+
+                    console.dir(error);
+                  });
+  }
+
+  /**
+   * Function that gets executed when there is no camera available on the users phone.
+   */
+  function notAvailableFunction() {
+    Dialog.alert({
+                   title: "Where is your camera?",
+                   message: "There is no camera available on your phone.",
+                   okButtonText: "Ok"
+                 });
+  }
 }
 
 /**
