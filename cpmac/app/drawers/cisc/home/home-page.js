6478896bce5e09b93c3a81cc9f3df07f16761bd6
caizan <vincent.petri@epfl.ch>
2017-12-13 13:37:14 +0100

    cisc: refacetored connect button

--- cpmac/app/drawers/cisc/home/home-page.js
+++ cpmac/app/drawers/cisc/home/home-page.js
@@ -9,6 +9,7 @@ const DedisCrypto = require("~/shared/lib/dedis-js/src/crypto");
 const FileIO = require("~/shared/lib/file-io/file-io");
 const FilePaths = require("~/shared/res/files/files-path");
 const HASH = require("hash.js");
+const BarcodeScanner = require("nativescript-barcodescanner").BarcodeScanner;
 
 let page;
 let viewmodel;
@@ -161,7 +162,78 @@ function GetByteArrayFromString(parameter) {
     return mainbytesArray;
 }
 
+function connectButtonTapped(args) {
+    const barcodescanner = new BarcodeScanner();
+
+    return barcodescanner.available().then(function (available) {
+        if (available) {
+            return availableFunction();
+        } else {
+            return notAvailableFunction();
+        }
+    });
+
+    function availableFunction () {
+        barcodescanner.scan({
+            formats: "QR_CODE", // Pass in of you want to restrict scanning to certain types
+            cancelLabel: "EXIT. Also, try the volume buttons!", // iOS only, default 'Close'
+            cancelLabelBackgroundColor: "#333333", // iOS only, default '#000000' (black)
+            message: "Use the volume buttons for extra light", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.'
+            showFlipCameraButton: true, // default false
+            preferFrontCamera: false, // default false
+            showTorchButton: true, // default false
+            beepOnScan: true, // Play or Suppress beep on scan (default true)
+            torchOn: false, // launch with the flashlight on (default false)
+            closeCallback: function () {
+                console.log("Scanner closed");
+            }, // invoked when the scanner was closed (success or abort)
+            resultDisplayDuration: 500, // Android only, default 1500 (ms), set to 0 to disable echoing the scanned text
+            orientation: "landscape", // Android only, optionally lock the orientation to either "portrait" or "landscape"
+            openSettingsIfPermissionWasPreviouslyDenied: true // On iOS you can send the user to the settings app if access was previously denied
+        }).then(
+            (result) => {
+                console.log(`Scan format: ${result.format}`);
+                console.log(`Scan text: ${result.text}`);
+                const splitColon = result.text.split(":");
+                const splitSlash = splitColon[2].split("/");
+                const goodURL = `tcp:${splitColon[1]}:${splitSlash[0]}`;
+                console.log(goodURL);
+                setTimeout(() => {
+                    const toWrite = `${goodURL}/${splitSlash[1]}`;
+                    FileIO.writeStringTo(FilePaths.CISC_IDENTITY_LINK, toWrite)
+                        .then(() => {
+                            setTimeout(()=> {
+                                viewModel.update();
+                            },100);
+                            console.log(`saved ${toWrite} in ${FilePaths.CISC_IDENTITY_LINK}`);
+                        });
+                    Dialog.alert({
+                        title:"Scan Successful",
+                        message:`connection to ${goodURL}`,
+                        okButtonText: "Ok"
+                    });
+                }, 100);
+            },
+            (error) => setTimeout(() => Dialog.alert({
+                title: "Scanner Error",
+                message: error,
+                okButtonText: "Ok"
+            }), 100)
+        );
+    }
+
+    function notAvailableFunction() {
+        return Dialog.alert({
+            title: "Where is your camera?",
+            message: "There is no camera available on your phone.",
+            okButtonText: "Ok"
+        });
+    }
+}
+
+
 exports.onLoaded = onLoaded;
 exports.toggleProposed = toggleProposed;
 exports.onDrawerButtonTap = onDrawerButtonTap;
 exports.voteForProposed = voteForProposed;
+exports.connectButtonTapped = connectButtonTapped;
--- cpmac/app/drawers/cisc/qr/qr-page.js
+++ cpmac/app/drawers/cisc/qr/qr-page.js
@@ -1,8 +1,4 @@
 const Frame = require("ui/frame");
-const Dialog = require("ui/dialogs");
-const BarcodeScanner = require("nativescript-barcodescanner").BarcodeScanner;
-const FileIO = require("~/shared/lib/file-io/file-io");
-const FilePaths = require("~/shared/res/files/files-path");
 const ZXing = require("nativescript-zxing");
 const ImageSource = require("image-source");
 const PlatformModule = require("tns-core-modules/platform");
@@ -64,76 +60,7 @@ function onDrawerButtonTap(args) {
     sideDrawer.showDrawer();
 }
 
-function connectButtonTapped(args) {
-    const barcodescanner = new BarcodeScanner();
-
-    return barcodescanner.available().then(function (available) {
-        if (available) {
-            return availableFunction();
-        } else {
-            return notAvailableFunction();
-        }
-    });
-
-    function availableFunction () {
-        barcodescanner.scan({
-            formats: "QR_CODE", // Pass in of you want to restrict scanning to certain types
-            cancelLabel: "EXIT. Also, try the volume buttons!", // iOS only, default 'Close'
-            cancelLabelBackgroundColor: "#333333", // iOS only, default '#000000' (black)
-            message: "Use the volume buttons for extra light", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.'
-            showFlipCameraButton: true, // default false
-            preferFrontCamera: false, // default false
-            showTorchButton: true, // default false
-            beepOnScan: true, // Play or Suppress beep on scan (default true)
-            torchOn: false, // launch with the flashlight on (default false)
-            closeCallback: function () {
-                console.log("Scanner closed");
-            }, // invoked when the scanner was closed (success or abort)
-            resultDisplayDuration: 500, // Android only, default 1500 (ms), set to 0 to disable echoing the scanned text
-            orientation: "landscape", // Android only, optionally lock the orientation to either "portrait" or "landscape"
-            openSettingsIfPermissionWasPreviouslyDenied: true // On iOS you can send the user to the settings app if access was previously denied
-        }).then(
-            (result) => {
-                console.log(`Scan format: ${result.format}`);
-                console.log(`Scan text: ${result.text}`);
-                const splitColon = result.text.split(":");
-                const splitSlash = splitColon[2].split("/");
-                const goodURL = `tcp:${splitColon[1]}:${splitSlash[0]}`;
-                console.log(goodURL);
-                setTimeout(() => {
-                    const toWrite = `${goodURL}/${splitSlash[1]}`;
-                    FileIO.writeStringTo(FilePaths.CISC_IDENTITY_LINK, toWrite)
-                        .then(() => {
-                            setTimeout(()=> {
-                                viewModel.update();
-                                updateImage();
-                            },100);
-                            console.log(`saved ${toWrite} in ${FilePaths.CISC_IDENTITY_LINK}`);
-                    });
-                    Dialog.alert({
-                        title:"Scan Successful",
-                        message:`connection to ${goodURL}`,
-                        okButtonText: "Ok"
-                    });
-                }, 100);
-            },
-            (error) => setTimeout(() => Dialog.alert({
-                title: "Scanner Error",
-                message: error,
-                okButtonText: "Ok"
-            }), 100)
-        );
-    }
-
-    function notAvailableFunction() {
-        return Dialog.alert({
-            title: "Where is your camera?",
-            message: "There is no camera available on your phone.",
-            okButtonText: "Ok"
-        });
-    }
-}
 
 exports.onLoaded = onLoaded;
 exports.onDrawerButtonTap = onDrawerButtonTap;
-exports.connectButtonTapped = connectButtonTapped;
+exports.updateImage = updateImage;
