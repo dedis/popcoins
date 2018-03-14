f69b9741fac903673d83859fcb0722646cc87a1e
Sacha Kozma <sacha.kozma@gmail.com>
2018-03-15 00:23:17 +0100

    Updated Makefile as protobufjs and proto buffers build are not required anympre

--- cpmac/prepare-hook.sh
+++ cpmac/prepare-hook.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-rm -f node_modules/protobufjs/dist/protobuf.min.js.gz
-rm -f platforms/android/src/main/assets/app/tns_modules/protobufjs/dist/protobuf.min.js.gz
-
-rm -f node_modules/protobufjs/dist/minimal/protobuf.min.js.gz
-rm -f platforms/android/src/main/assets/app/tns_modules/protobufjs/dist/minimal/protobuf.min.js.gz
-
-rm -f node_modules/protobufjs/dist/light/protobuf.min.js.gz
-rm -f platforms/android/src/main/assets/app/tns_modules/protobufjs/dist/light/protobuf.min.js.gz
-
 rm -f node_modules/long/dist/long.min.js.gz
 rm -f platforms/android/src/main/assets/app/tns_modules/long/dist/long.min.js.gz
 
