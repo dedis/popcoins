5b6c2a9c766f2ad007c57f3459fa987fc8708e24
Claudio5 <claudio.loureiro1@gmail.com>
2018-06-03 17:44:33 +0200

    Added verify functionality for certificate

--- cpmac/app/drawers/cisc/cert/cert-details/cert-details.js
+++ cpmac/app/drawers/cisc/cert/cert-details/cert-details.js
@@ -1,19 +1,50 @@
 const FrameModule = require("ui/frame");
 const ObservableArray = require("data/observable-array").ObservableArray;
 const ObservableModule = require("data/observable");
+const topmost = require("ui/frame").topmost;
 const merge = require("node.extend");
 const common = require("asn1js/org/pkijs/common");
 const _asn1js = require("asn1js");
 const _pkijs = require("pkijs");
 const _x509schema = require("pkijs/org/pkijs/x509_schema");
+const pki = require('node-forge').pki;
+const Dialog = require("ui/dialogs");
 
-let conode = undefined;
-let conodeStatus = undefined;
-let pageObject = undefined;
+const caCert = "-----BEGIN CERTIFICATE-----\
+MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\
+MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\
+DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\
+SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\
+GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\
+AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\
+q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\
+SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\
+Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\
+a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\
+/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\
+AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\
+CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\
+bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\
+c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\
+VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\
+ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\
+MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\
+Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\
+AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\
+uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\
+wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\
+X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\
+PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\
+KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\
+-----END CERTIFICATE-----";
+
+
 let viewModel = ObservableModule.fromObject({
 	certName: "",
     certInfos: new ObservableArray()
 });
+
+let certStored;
 
 function onNavigatingTo(args) {
   	if (args.isBackNavigation) {
@@ -23,8 +54,8 @@ function onNavigatingTo(args) {
     page = args.object;
 
     const context = page.navigationContext;
-
-    const result = getBasicInfo(context.cert);
+    certStored = context.cert;
+    const result = getBasicInfo(certStored);
 
     page.bindingContext = viewModel
 
@@ -32,6 +63,27 @@ function onNavigatingTo(args) {
     if(viewModel.certInfos.length == 0){
        loadCertInfos(result)
     }
+}
+
+function verifyCert(args){
+
+    const cert = pki.certificateFromPem(certStored);
+    caStore = pki.createCaStore([ caCert ]);
+
+    try {
+        pki.verifyCertificateChain(caStore, [cert]);
+    } catch (e) {
+        return Dialog.alert({
+            title: "Verify",
+            message: "Certificate is not valid",
+            okButtonText: "Ok"
+        })
+    }
+    return Dialog.alert({
+            title: "Verify",
+            message: "Certificate is valid",
+            okButtonText: "Ok"
+    })
 }
 
 function loadCertInfos(certInfos){
@@ -84,11 +136,11 @@ function getBasicInfo(pem) {
     var sub;
 
     c.extensions.forEach(function (ext) {
-    if (ext.parsedValue && ext.parsedValue.altNames) {
-        ext.parsedValue.altNames.forEach(function (alt) {
-        domains.push(alt.Name);
-        });
-    }
+        if (ext.parsedValue && ext.parsedValue.altNames) {
+                ext.parsedValue.altNames.forEach(function (alt) {
+                domains.push(alt.Name);
+            });
+        }
     });
 
     sub = c.subject.types_and_values[0].value.value_block.value || null;
@@ -108,5 +160,11 @@ function onDrawerButtonTap(args) {
     sideDrawer.showDrawer();
 }
 
+function goBack() {
+    topmost().goBack();
+}
+
+exports.goBack = goBack;
+exports.verifyCert = verifyCert;
 exports.onNavigatingTo = onNavigatingTo;
 exports.onDrawerButtonTap = onDrawerButtonTap;
