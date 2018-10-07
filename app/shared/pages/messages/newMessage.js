5fd8341c2b0131ecb974efb517740258cab8e0dd
Linus Gasser <linus.gasser@epfl.ch>
2018-10-07 15:44:16 +0200

    too much cleanup

---
+++ app/shared/pages/messages/newMessage.js
@@ -0,0 +1,48 @@
+const Dialog = require("ui/dialogs");
+const Helper = require("../../lib/dedjs/Helper");
+const NetUtils = require("../../lib/dedjs/network/NetUtils");
+const ObservableModule = require("data/observable");
+
+const viewModel = ObservableModule.fromObject({
+  newMessage: ObservableModule.fromObject({
+      subject: "",
+      text: "",
+      balance: 100000,
+      reward: 20000,
+  }),
+  isLoading: false,
+  isEmpty: true
+});
+
+let closeCallBackFunction = undefined;
+
+let page = undefined;
+
+function onShownModally(args) {
+    closeCallBackFunction = args.closeCallback;
+}
+
+function onLoaded(args) {
+  page = args.object;
+
+  page.bindingContext = viewModel;
+}
+
+function onUnloaded() {
+  // remove polling when page is leaved
+  // Timer.clearInterval(timerId);
+}
+
+function addManual(){
+    closeCallBackFunction(viewModel.newMessage);
+}
+
+function onCancel(){
+    closeCallBackFunction();
+}
+
+module.exports.addManual = addManual;
+module.exports.onCancel = onCancel;
+module.exports.onLoaded = onLoaded;
+module.exports.onUnloaded = onUnloaded;
+module.exports.onShownModally = onShownModally;
